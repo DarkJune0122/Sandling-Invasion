6a9d78e604d7d869fb8629711084afda5a3c5f23
DarkJune <56516330+DarkJune0122@users.noreply.github.com>
2025-08-06 00:13:31 +0300

    Exploring the code. Working on pet item.

--- Plugin.cs
+++ Plugin.cs
@@ -16,9 +16,30 @@ namespace SandlingInvasion
     {
         public const string GUID = "sandlings-united.etg.sandling-invasion";
         public const string NAME = "Sandling Invasion!";
+        public const string API = "SandlingAPI";
         public const string VERSION = "0.1.0";
         public const string TEXT_COLOR = "#FFD97F";
 
+
+
+
+
+        /// ===     ===     ===     ===    ===  == =  -                        -  = ==  ===    ===     ===     ===     ===<![CDATA[
+        /// .
+        /// .                                              Static Properties
+        /// .
+        /// ===     ===     ===     ===    ===  == =  -                        -  = ==  ===    ===     ===     ===     ===]]>
+        public static bool Initialized { get; set; }
+
+
+
+
+
+        /// ===     ===     ===     ===    ===  == =  -                        -  = ==  ===    ===     ===     ===     ===<![CDATA[
+        /// .
+        /// .                                               Public Methods
+        /// .
+        /// ===     ===     ===     ===    ===  == =  -                        -  = ==  ===    ===     ===     ===     ===]]>
         public void Start()
         {
             ETGModMainBehaviour.WaitForGameManagerStart(GMStart);
@@ -26,13 +47,51 @@ namespace SandlingInvasion
 
         public void GMStart(GameManager g)
         {
-            ExamplePassive.Register();
-            Log($"{NAME} v{VERSION} started successfully.", TEXT_COLOR);
+            try
+            {
+                RegisterAll();
+            }
+            catch (Exception e)
+            {
+                Log($"{NAME} v{VERSION} was NOT loaded successfully! See the exception:\n" + e.Message, "#FF1212");
+            }
+            finally
+            {
+                Log($"{NAME} v{VERSION} started successfully.", TEXT_COLOR);
+            }
+        }
+
+
+
+
+
+        /// ===     ===     ===     ===    ===  == =  -                        -  = ==  ===    ===     ===     ===     ===<![CDATA[
+        /// .
+        /// .                                               Static Methods
+        /// .
+        /// ===     ===     ===     ===    ===  == =  -                        -  = ==  ===    ===     ===     ===     ===]]>
+        public static void RegisterAll()
+        {
+            if (Initialized) return;
+
+            // Registers items:
+            Sandling.Register();
+
+            // Should it be moved to the beginning of the registration?
+            Initialized = true;
         }
 
         public static void Log(string text, string color="#FFFFFF")
         {
-            ETGModConsole.Log($"<color={color}>{text}</color>");
+            if (text.Contains('\n'))
+            {
+                foreach (var line in text.Split('\n', '\r'))
+                {
+                    if (string.IsNullOrEmpty(line)) continue;
+                    else ETGModConsole.Log($"<color={color}>{line}</color>");
+                }
+            }
+            else ETGModConsole.Log($"<color={color}>{text}</color>");
         }
     }
 }
---
+++ Sandling.cs
@@ -0,0 +1,73 @@
+﻿using Alexandria.ItemAPI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace SandlingInvasion;
+
+public class Sandling : PassiveItem
+{
+    /// ===     ===     ===     ===    ===  == =  -                        -  = ==  ===    ===     ===     ===     ===<![CDATA[
+    /// .
+    /// .                                               Static Fields
+    /// .
+    /// ===     ===     ===     ===    ===  == =  -                        -  = ==  ===    ===     ===     ===     ===]]>
+    public const string ItemName = "Sandling";
+    public const string ShortDescription = "A <i>friendly</i> sandling that follows you around.";
+    public const string LongDescription = "A <i>friendly</i> sandling that follows you around. ------------------";
+
+
+
+
+
+    /// ===     ===     ===     ===    ===  == =  -                        -  = ==  ===    ===     ===     ===     ===<![CDATA[
+    /// .
+    /// .                                               Static Methods
+    /// .
+    /// ===     ===     ===     ===    ===  == =  -                        -  = ==  ===    ===     ===     ===     ===]]>
+    public static void Register()
+    {
+        // Creates the item.
+        const string ItemSpritePath = $"{nameof(SandlingInvasion)}/Resources/sandling-item.png";
+        GameObject obj = new(ItemName);
+        var item = obj.AddComponent<Sandling>();
+
+        ItemBuilder.AddSpriteToObject(ItemName, ItemSpritePath, obj);
+
+        // Registers the items.
+        ItemBuilder.SetupItem(item, ShortDescription, LongDescription, Plugin.API);
+        item.encounterTrackable.prerequisites = [];
+
+        // Testing:
+        ItemBuilder.AddPassiveStatModifier(item, PlayerStats.StatType.Health, 1, StatModifier.ModifyMethod.ADDITIVE);
+        ItemBuilder.AddPassiveStatModifier(item, PlayerStats.StatType.Coolness, 1);
+        item.quality = ItemQuality.C;
+
+        // Replaces a built-in Dog item in character's inventory.
+        //AIActor actor = EnemyDatabase.GetOrLoadByGuid("c80f57241dcf4fc3a4c33998141f4394");
+        //Plugin.Log(actor == null ? "Actor not found." : $"Actor {actor.ActorName} found.");
+    }
+
+
+
+
+
+    /// ===     ===     ===     ===    ===  == =  -                        -  = ==  ===    ===     ===     ===     ===<![CDATA[
+    /// .
+    /// .                                               Public Methods
+    /// .
+    /// ===     ===     ===     ===    ===  == =  -                        -  = ==  ===    ===     ===     ===     ===]]>
+    public override void Pickup(PlayerController player)
+    {
+        base.Pickup(player);
+        Plugin.Log($"Player picked up {DisplayName}");
+    }
+
+    public override void DisableEffect(PlayerController player)
+    {
+        // TODO: Add full-screen no-respect warning.
+        Plugin.Log($"Player dropped or got rid of {DisplayName}! NO RESPECT!");
+    }
+}
